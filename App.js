e15559eb0ec53d1cdef1428ae602462e021fbb21
Eddy Caldas <eddycaldas@live.com>
2019-03-13 21:17:12 -0600

    add close and delete modal

--- App.js
+++ App.js
@@ -1,46 +1,73 @@
 
-import React, {Component} from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
 
-import PlaceInput from './src/components/PlaceInput/PlaceInput';
-import PlaceList from './src/components/PlaceList/PlaceList';
-import workerImage from './src/assets/image.jpg';
+import PlaceInput from "./src/components/PlaceInput/PlaceInput";
+import PlaceList from "./src/components/PlaceList/PlaceList";
+import PlaceDetail from "./src/components/PlaceDetail/PlaceDetail";
 
 export default class App extends Component {
   state = {
-    places: []
-  }
+    places: [],
+    selectedPlace: null
+  };
 
-  placeAddedHandler = (placeName) => {
+  placeAddedHandler = placeName => {
     this.setState(prevState => {
       return {
         places: prevState.places.concat({
-        key: Math.random(), 
-        name: placeName,
-        image: workerImage
+          key: Math.random(),
+          name: placeName,
+          image: {
+            uri:
+              "https://c1.staticflickr.com/5/4096/4744241983_34023bf303_b.jpg"
+          }
         })
-      }
-    })
-  }
+      };
+    });
+  };
 
-  placeDeletedHandler = (key) => {
+  placeDeletedHandler = () => {
     this.setState(prevState => {
       return {
         places: prevState.places.filter(place => {
-          return place.key !== key;
-      })
-      }
-    })
-  }
+          return place.key !== prevState.selectedPlace.key;
+        }),
+        selectedPlace: null
+      };
+    });
+  };
+
+  modalClosedHandler = () => {
+    this.setState({
+      selectedPlace: null
+    });
+  };
+
+  placeSelectedHandler = key => {
+    this.setState(prevState => {
+      return {
+        selectedPlace: prevState.places.find(place => {
+          return place.key === key;
+        })
+      };
+    });
+  };
 
   render() {
     return (
       <View style={styles.container}>
-        <PlaceInput onPlaceAdded={this.placeAddedHandler}/>
-        <PlaceList 
+        <PlaceDetail
+          selectedPlace={this.state.selectedPlace}
+          onItemDeleted={this.placeDeletedHandler}
+          onModalClosed={this.modalClosedHandler}
+        />
+        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
+        <PlaceList
           places={this.state.places}
-          onItemDeleted={this.placeDeletedHandler}/>
-        </View>
+          onItemSelected={this.placeSelectedHandler}
+        />
+      </View>
     );
   }
 }
@@ -48,9 +75,9 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 40,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    padding: 26,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start"
   }
 });
---
+++ src/components/PlaceDetail/PlaceDetail.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {Modal, View, Text, Button, Image, StyleSheet} from 'react-native';
+
+const placeDetail = (props) => {
+    let modalContent = null;
+
+    if(props.selectedPlace) {
+     modalContent = (
+            <View>
+                <Image 
+                    source={props.selectedPlace.image}
+                    style={styles.placeImage}/>
+                <Text 
+                    style={styles.placeName}>                     {props.selectedPlace.name}
+                </Text>
+            </View>                   
+     )   
+    }
+
+    return (
+        <Modal visible={props.selectedPlace !== null}
+                animationType="slide">
+            <View style={styles.modalContainer}>
+                {modalContent}
+                <View>
+                    <Button 
+                        title="Delete" color="red"
+                        onPress={props.onItemDeleted}/>                
+                    <Button 
+                        title="Close"
+                        onPress={props.onModalClosed}/>
+                </View>
+            </View>
+        </Modal>
+    )
+};
+
+const styles = StyleSheet.create({
+    modalContainer: {
+        margin: 32
+    },
+    placeImage: {
+        width: "100%",
+        height: 200
+    },
+    placeName: {
+        fontSize: 28
+        }
+})
+
+export default placeDetail;
